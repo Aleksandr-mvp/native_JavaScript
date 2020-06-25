2139f17a3e008048a7936a406c41caba6ef50d25
Aleksandr Pushnin <deadreebook@gmail.com>
2020-06-25 13:46:48 +0800

    added variable

---
+++ variable/variable.js
@@ -0,0 +1,17 @@
+"use strict";
+
+// 1 decision
+let admin;
+let name = "Jon";
+
+// 2 decision
+admin = name;
+alert(admin);
+
+// 3 decision
+let ourPlanetName = "Earth";
+let currentUserName = "Jon";
+
+
+
+
