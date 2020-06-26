4e45b23c6f5d5c55a8721500f6f6e82cff00791a
Aleksandr Pushnin <deadreebook@gmail.com>
2020-06-26 13:53:03 +0800

    added conditionalStatements

---
+++ conditionalStatements/conditionalStatements.js
@@ -0,0 +1,30 @@
+"use strict";
+
+// 1 decision
+let languageName = prompt('Какое официальное название языка JavaScript?');
+
+if (languageName == 'ECMAScript') {
+    alert('Правильно!');
+} else {
+    alert('Ты не знаешь? «ECMAScript»!');
+}
+
+// 2 decision
+let signNumber = prompt('Введите число');
+
+if (signNumber > 0) {
+    alert(1);
+} else if (signNumber < 0) {
+    alert(-1);
+} else {
+    alert(0);
+}
+
+// 2 decision
+let result = (a + b < 4) ? 'Мало' : 'Много';
+
+// 2 decision
+let message = (login == 'Сотрудник') ? 'Привет' :
+    (login == 'Директор') ? 'Здравствуйте' :
+    (login == '') ? 'Нет логина' : '';
+
